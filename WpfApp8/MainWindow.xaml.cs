2978e2ae75b2a3969679ebe4cae80e82891a9bc0
darkj <darkj@LAPTOP-KGK1F842>
2024-04-22 19:53:34 +0200

    Přidejte soubory projektu.

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SpravceUkolu spravceUkolu;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            spravceUkolu = new SpravceUkolu(); // Inicializace správce úkolů
+            lstUkoly.ItemsSource = spravceUkolu.Ukoly; // Nastavení zdroje pro ListBox
+        }
+
+        private void BtnPridatUkol_Click(object sender, RoutedEventArgs e)
+        {
+            string nazevUkolu = txtNovyUkol.Text;
+            spravceUkolu.PridatUkol(nazevUkolu); // Přidání nového úkolu pomocí správce úkolů
+            txtNovyUkol.Text = ""; // Vymaž textové pole po přidání úkolu
+        }
+
+        private void BtnOznacitHotovo_Click(object sender, RoutedEventArgs e)
+        {
+            Ukol vybranyUkol = lstUkoly.SelectedItem as Ukol;
+            if (vybranyUkol != null)
+            {
+                spravceUkolu.OznacitUkolHotovo(vybranyUkol); // Označení úkolu jako hotového pomocí správce úkolů
+                AktualizovatSeznamUkolu();
+            }
+            else
+            {
+                MessageBox.Show("Vyberte úkol, který chcete označit jako hotový.", "Upozornění", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnOdstranitUkol_Click(object sender, RoutedEventArgs e)
+        {
+            Ukol vybranyUkol = lstUkoly.SelectedItem as Ukol;
+            if (vybranyUkol != null)
+            {
+                spravceUkolu.OdstranitUkol(vybranyUkol); // Odstranění úkolu pomocí správce úkolů
+                AktualizovatSeznamUkolu();
+            }
+            else
+            {
+                MessageBox.Show("Vyberte úkol, který chcete odstranit.", "Upozornění", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void AktualizovatSeznamUkolu()
+        {
+            // Projdeme všechny položky v ListBoxu
+            foreach (var item in lstUkoly.Items)
+            {
+                // Získáme ListBoxItem z položky
+                var listBoxItem = lstUkoly.ItemContainerGenerator.ContainerFromItem(item) as ListBoxItem;
+                if (listBoxItem != null)
+                {
+                    // Ručně aktualizujeme styl podle hodnoty JeHotovo
+                    var u = item as Ukol;
+                    if (u != null && u.JeHotovo)
+                    {
+                        listBoxItem.Background = Brushes.LightGreen;
+                    }
+                    else
+                    {
+                        listBoxItem.Background = Brushes.Transparent;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ WpfApp8/SpravceUkolu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8
+{
+    public class SpravceUkolu
+    {
+        public ObservableCollection<Ukol> Ukoly { get; set; }
+
+        public SpravceUkolu()
+        {
+            Ukoly = new ObservableCollection<Ukol>();
+        }
+
+        public void PridatUkol(string nazev)
+        {
+            Ukol novyUkol = new Ukol(nazev);
+            Ukoly.Add(novyUkol);
+            Console.WriteLine("Nový úkol přidán: " + nazev); // Kontrolní výpis do konzole
+        }
+
+        public void OznacitUkolHotovo(Ukol ukol)
+        {
+            ukol.JeHotovo = true;
+        }
+
+        public void OdstranitUkol(Ukol ukol)
+        {
+            Ukoly.Remove(ukol);
+        }
+    }
+
+    public class Ukol
+    {
+        public string Nazev { get; set; }
+        public bool JeHotovo { get; set; }
+
+        public Ukol(string nazev)
+        {
+            Nazev = nazev;
+            JeHotovo = false;
+        }
+    }
+}
